4826c4b2f71dacc5aa95deb4b0aa96db98fe6047
darkside1337 <107885019+darkside1337@users.noreply.github.com>
2024-07-11 18:22:52 +0100

    scaffold UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "~/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,12 +8,24 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <div>Sign In</div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className="scroll-smooth antialiased">
+      <body className="flex flex-col gap-4">
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/aff0c210-ee08-430f-831b-9eed01d00a9c-lcm5kk.svg",
+  "https://utfs.io/f/8a10868e-90a8-4fa2-ba0e-601b0c3ed447-kl4egl.svg",
+  "https://utfs.io/f/d7fa2c91-b1b9-4159-9a34-4fc7232b7326-j3g8bl.svg",
+  "https://utfs.io/f/8326b611-8bba-4bd9-a4fd-443da1ce8f30-l7xosj.svg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       hello gallery in progress
     </main>
   );
