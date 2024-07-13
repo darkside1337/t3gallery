547a2add8f327e3cad67e3a6252dd54b7cc8135f
darkside1337 <107885019+darkside1337@users.noreply.github.com>
2024-07-13 23:33:40 +0100

    homepage auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,37 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image) => (
+        <div key={image.id} className="flex w-48 flex-col gap-2">
+          <Image src={image.url} alt={image.name} width={192} height={192} />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image) => (
-          <div key={image.id} className="flex w-48 flex-col gap-2">
-            <Image src={image.url} alt={image.name} width={192} height={192} />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
-      hello gallery in progress
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above :)
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
