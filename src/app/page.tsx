26d07049497c5ecf462796c6f1d76f1671a4b335
darkside1337 <107885019+darkside1337@users.noreply.github.com>
2024-07-13 19:43:11 +0100

    DB Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/aff0c210-ee08-430f-831b-9eed01d00a9c-lcm5kk.svg",
   "https://utfs.io/f/8a10868e-90a8-4fa2-ba0e-601b0c3ed447-kl4egl.svg",
@@ -10,15 +12,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+        {posts.map((p) => (
+          <div key={p.name}>{p.name}</div>
+        ))}
+        {/*  {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
           </div>
-        ))}
+        ))} */}
       </div>
       hello gallery in progress
     </main>
