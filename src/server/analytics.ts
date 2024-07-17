7d15e13573e2c8621df6c77edd3d12909b91c1e8
darkside1337 <107885019+darkside1337@users.noreply.github.com>
2024-07-18 00:58:26 +0100

    add server only to analytics

--- src/server/analytics.ts
+++ src/server/analytics.ts
@@ -1,4 +1,7 @@
+import "server-only";
+
 import { PostHog } from "posthog-node";
+
 export function serverSideAnalytics() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
